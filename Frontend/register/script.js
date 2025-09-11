dc07aa75b8c336f37531ba2f49c5ed793f9c8aa4
RubeldiRubelda <benjamin.kunz.ch@gmail.com>
2025-09-11 16:26:06 +0200

    Account

---
+++ Frontend/register/script.js
@@ -0,0 +1,43 @@
+let baseurl = "https://server5.techsvc.de:2005"
+
+
+document.querySelector(".login-form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const username = e.target[0].value.trim();
+  const password = e.target[1].value;
+
+  if (!username || !password) {
+    alert("Bitte Benutzername und Passwort eingeben.");
+    return;
+  }
+
+  const combined = `${username}:${password}`;
+  const hash = await sha256(combined);
+
+  try {
+    // Statt JSON bekommst du nun HTML zurück
+    const response = await fetch(baseurl + "/api/user/login/" + username + "/" + hash);
+
+    if (response.Okay) {
+        document.cookie = "username=" + username + "; path=/";
+        document.cookie = "password=" + hash + "; path=/";
+        window.location.href = "/user";
+    } else {
+      alert("Zugang verweigert");
+    }
+  } catch (error) {
+    console.error("Fehler beim Login:", error);
+    alert("Login fehlgeschlagen.");
+  }
+
+});
+
+
+async function sha256(message) {
+  const msgBuffer = new TextEncoder().encode(message);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+  return hashHex;
+}
