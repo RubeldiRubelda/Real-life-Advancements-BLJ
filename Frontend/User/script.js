1afb7f3f65bb0b7eb69e4c744a192cfc7b381f57
The12Forest <manuel@steka.ch>
2025-09-11 14:26:38 +0200

    Update id in user fro username

--- Frontend/User/script.js
+++ Frontend/User/script.js
@@ -5,7 +5,7 @@ let baseurl = "https://server5.techsvc.de:2005"
 let Username = document.cookie.split('; ').find(row => row.startsWith('username='))?.split('=')[1];
 let Password = document.cookie.split('; ').find(row => row.startsWith('password='))?.split('=')[1];
 
-let div = document.getElementById("username-of-user");
+let div = document.getElementById("usernameofuser");
 div = Username.charAt(0).toUpperCase() + variable.slice(1);
 
 
