15e27470c58163e0d2c2f9231608ad71f18a7fab
The12Forest <manuel@steka.ch>
2025-09-11 15:17:20 +0200

    Test 3

--- Frontend/User/script.js
+++ Frontend/User/script.js
@@ -15,18 +15,20 @@ usernamediv.innerHTML = Username.charAt(0).toUpperCase() + Username.slice(1);
 let tasksdiv = document.getElementById("Container");
 
 
-tasksdiv.innerHTML += '         <div class="col-lg-2 col-sm-6">                              '
-tasksdiv.innerHTML += '            <div class="item">                                        '
-tasksdiv.innerHTML += '              <div class="icon">                                      '
-tasksdiv.innerHTML += '                <img src="assets/images/icon-06.png" alt="">          '
-tasksdiv.innerHTML += '              </div>                                                  '
-tasksdiv.innerHTML += '              <h4>Crypto Wallet</h4>                                  '
-tasksdiv.innerHTML += '              <h5 class="minifont">#Scammer</h5>                      '
-tasksdiv.innerHTML += '              <div class="icon-button">                               '
-tasksdiv.innerHTML += '                <a href="#"><i class="fa fa-angle-right"></i></a>     '
-tasksdiv.innerHTML += '              </div>                                                  '
-tasksdiv.innerHTML += '            </div>                                                    '
-tasksdiv.innerHTML += '        </div>                                                        '
+tasksdiv.innerHTML += `
+    <div class="col-lg-2 col-sm-6">
+    <div class="item">
+        <div class="icon">
+        <img src="assets/images/icon-06.png" alt="">
+        </div>
+        <h4>Crypto Wallet</h4>
+        <h5 class="minifont">#Scammer</h5>
+        <div class="icon-button">
+        <a href="#"><i class="fa fa-angle-right"></i></a>
+        </div>
+    </div>
+    </div>
+`;
 
 
 
