c983aa60828058548ee7f8c56b4f353113924dd0
The12Forest <manuel@steka.ch>
2025-09-11 14:58:57 +0200

    restore no restore

--- Frontend/User/script.js
+++ Frontend/User/script.js
@@ -1,5 +1,5 @@
 let baseurl = "https://server5.techsvc.de:2005"
-let div
+
 // let baseurl = "https://localhost:2005"
 
 // Load all data
@@ -7,27 +7,12 @@ let Username = document.cookie.split('; ').find(row => row.startsWith('username=
 let Password = document.cookie.split('; ').find(row => row.startsWith('password='))?.split('=')[1];
 
 // Add Username
-div = document.getElementById("usernameofuser");
+let div = document.getElementById("usernameofuser");
 div.innerHTML = Username.charAt(0).toUpperCase() + Username.slice(1);
 
 
 
-div = document.getElementById("Container");
 
-
-div.innerHTML +=         <div class="col-lg-2 col-sm-6">
-div.innerHTML +=             <div class="item">
-div.innerHTML +=             <div class="icon">
-div.innerHTML +=                 <img src="assets/images/icon-06.png" alt="">
-div.innerHTML +=             </div>
-div.innerHTML +=             <h4>Crypto Wallet</h4>
-div.innerHTML +=             <h5 class="minifont">#Scammer</h5>
-div.innerHTML +=             <div class="icon-button">
-div.innerHTML +=                 <a href="#"><i class="fa fa-angle-right"></i></a>
-div.innerHTML +=             </div>
-div.innerHTML +=             </div>
-div.innerHTML +=         </div>
-          
 
 
 
