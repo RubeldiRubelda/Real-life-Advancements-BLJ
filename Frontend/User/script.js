dcc2c39deb61d9570b46fd7d8a5f4d32dde67d32
The12Forest <manuel@steka.ch>
2025-09-12 08:53:22 +0200

    Test Task containers

--- Frontend/User/script.js
+++ Frontend/User/script.js
@@ -1,6 +1,6 @@
-let baseurl = "https://server5.techsvc.de:2005"
+// let baseurl = "https://server5.techsvc.de:2005"
 
-// let baseurl = "https://localhost:2005"
+let baseurl = "https://localhost:2005"
 
 // Load all data
 let Username = document.cookie.split('; ').find(row => row.startsWith('username='))?.split('=')[1];
@@ -12,23 +12,54 @@ usernamediv.innerHTML = Username.charAt(0).toUpperCase() + Username.slice(1);
 
 
 
-let tasksdiv = document.getElementById("Container");
+
+async function load_tasks() {
+  try {
+    let response = await fetch(basedomain + "/api/task/all/" + Username + "/" + (new Date().getDay()))
+    response = await response.json()
+
+    let tasksdiv = document.getElementById("Container");
+    if (Array.isArray(response)) {
+      if (response.length == 0) {
+        tasksdiv.textContent = "No tasks found.";
+        return;
+      }
+
+      let i = 0
+      tasksdiv.innerHTML = `
+          <div class="section-heading">
+            <div class="line-dec"></div>
+            <h2>Aktive <em>Achievements Test</em>:</h2>
+          </div>`;
+      while (i < response.length) {
+        tasksdiv.innerHTML += `
+            <div class="col-lg-2 col-sm-6">
+            <div class="item">
+                <div class="icon">
+                    <img src="${IMAGE}" alt="">
+                </div>
+                <h4>${Name}</h4>
+                <h5 class="minifont">${Discription}</h5>
+                <div class="icon-button">
+                    <a href="${i}"><i class="fa fa-angle-right"></i></a>
+                </div>
+            </div>
+            </div>
+        `;
+        i++
+      }
+    } else {
+      console.error("Not array for tasks returned: ", response.Error)
+    }
+  } catch (error) {
+    document.getElementById('task-list').textContent = "Failed to load tasks.";
+    console.error(error);
+  }
 
 
-tasksdiv.innerHTML += `
-    <div class="col-lg-2 col-sm-6">
-    <div class="item">
-        <div class="icon">
-        <img src="assets/images/icon-06.png" alt="">
-        </div>
-        <h4>Crypto Wallet</h4>
-        <h5 class="minifont">#Scammer</h5>
-        <div class="icon-button">
-        <a href="#"><i class="fa fa-angle-right"></i></a>
-        </div>
-    </div>
-    </div>
-`;
+}
+
+
 
 
 
