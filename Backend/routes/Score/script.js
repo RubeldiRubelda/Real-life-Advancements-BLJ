4a86aeec21e08c03aca9375afd742a45ca9530c1
The12Forest <manuel@steka.ch>
2025-09-12 10:12:34 +0200

    Update User Task Cards

--- Backend/routes/Score/script.js
+++ Backend/routes/Score/script.js
@@ -2,25 +2,31 @@ import express from "express"
 import fs from 'fs';
 const router = express.Router()
 const logprefix = "ScoreRouter:            "
+let scores = []
+let users = []
 
 router.use("/save", (req, res) => {
-  let buffer = JSON.stringify(tasks)
+  let buffer = JSON.stringify(scores)
   fs.writeFileSync("./Backend/saves/tasks.json", buffer)
-  console.log(logprefix + "Settings tasks saved:      " + buffer)
-  buffer = JSON.stringify(task_times)
-  fs.writeFileSync("./Backend/saves/task_times.json", buffer)
-  console.log(logprefix + "Settings times saved:      " + buffer)
-  res.json(buffer)
+  console.log(logprefix + "Scores tasks saved:     " + buffer)
+  res.json({"Okay": true, "Discription": "Scores Saved"})
+  
 })
 
 router.use("/load", (req, res) => {
-  task_times = JSON.parse(fs.readFileSync("./Backend/saves/task_times.json"))
-  tasks = JSON.parse(fs.readFileSync("./Backend/saves/tasks.json"))
+  scores = JSON.parse(fs.readFileSync("./Backend/saves/tasks.json"))
   users = JSON.parse(fs.readFileSync("./Backend/saves/user.json"))
-  console.log(logprefix + "Times loaded:         " + JSON.stringify(task_times))
-  console.log(logprefix + "Tasks loaded:         " + JSON.stringify(tasks))
+  console.log(logprefix + "Scores loaded:        " + JSON.stringify(scores))
   console.log(logprefix + "Users loaded:         " + JSON.stringify(users))
-  res.json(tasks)
+  res.json({"Okay": true, "Discription": "Settings Loaded"})
+})
+
+router.use("/scoretr", (req, res) => {
+  scores = JSON.parse(fs.readFileSync("./Backend/saves/tasks.json"))
+  users = JSON.parse(fs.readFileSync("./Backend/saves/user.json"))
+  console.log(logprefix + "Scores loaded:        " + JSON.stringify(scores))
+  console.log(logprefix + "Users loaded:         " + JSON.stringify(users))
+  res.json({"Okay": true, "Discription": "Settings Loaded"})
 })
 
 
--- Frontend/User/script.js
+++ Frontend/User/script.js
@@ -6,57 +6,63 @@ let baseurl = "https://server5.techsvc.de:2005"
 let Username = document.cookie.split('; ').find(row => row.startsWith('username='))?.split('=')[1];
 let Password = document.cookie.split('; ').find(row => row.startsWith('password='))?.split('=')[1];
 
+if (Username == "") {
+    window.location.href = '/login'
+} else {
+    console.log("Username: " + Username)
+}
+
 // Add Username
 let usernamediv = document.getElementById("usernameofuser");
 usernamediv.innerHTML = Username.charAt(0).toUpperCase() + Username.slice(1);
 
 
+
+
+
 async function load_tasks() {
-    try {
-        let names = await fetch(baseurl + "/api/TDB/get/name")
-        let disciptions = await fetch(baseurl + "/api/TDB/get/discription")
-        let image = await fetch(baseurl + "/api/TDB/get/image")
-        // let response = await fetch(baseurl + "/api/TDB/" +  Username + "/" + Password + "/" + )
-        // response = await response.json()
-        names = await names.json()
-        disciptions = await disciptions.json()
-        image = await image.json()
 
-        let tasksdiv = document.getElementById("Container");
-        if (names.length == 0) {
-        tasksdiv.textContent = "No tasks found.";
-        return;
-        }
+    let names = await fetch(baseurl + "/api/TDB/get/name")
+    let hashtag = await fetch(baseurl + "/api/TDB/get/hashtag")
+    let image = await fetch(baseurl + "/api/TDB/get/image")
+    let description = await fetch(baseurl + "/api/TDB/get/description")
+    // let response = await fetch(baseurl + "/api/TDB/" +  Username + "/" + Password + "/" + )
+    // response = await response.json()
+    names = await names.json()
+    hashtag = await hashtag.json()
+    image = await image.json()
+    description = await description.json()
 
-        let i = 0
-        tasksdiv.innerHTML = `
-            <div class="section-heading">
-            <div class="line-dec"></div>
-            <h2>Aktive <em>Achievements Test</em>:</h2>
-            </div>`;
-        while (i < names.length) {
-        tasksdiv.innerHTML += `
-            <div class="col-lg-2 col-sm-6">
-            <div class="item">
-                <div class="icon">
-                    <img src="${image[i]}" alt="">
-                </div>
-                <h4>${names[i]}</h4>
-                <h5 class="minifont">${disciptions[i]}</h5>
-                <div class="icon-button">
-                    <a href="${i}"><i class="fa fa-angle-right"></i></a>
-                </div>
+
+
+    let tasksdiv = document.getElementById("Container");
+    if (names.length == 0) {
+    tasksdiv.textContent = "No tasks found.";
+    return;
+    }
+
+    let i = 0
+    tasksdiv.innerHTML = `
+        <div class="line-dec section-heading"></div>
+        <h2>Aktive <em>Achievements</em>:</h2>
+        </div>`;
+    while (i < names.length) {
+    tasksdiv.innerHTML += `
+        <div class="col-lg-2 col-sm-6">
+        <div class="item">
+            <div class="icon">
+                <img src="${image[i]}" alt="">
             </div>
+            <h4>${names[i]}</h4>
+            <h5 class="minifont">${hashtag[i]}</h5>
+            <div class="icon-button">
+                <a href="${i}"><i class="fa fa-angle-right"></i></a>
             </div>
-            `;
-            i++
-        }
-  } catch (error) {
-    document.getElementById('task-list').textContent = "Failed to load tasks.";
-    console.error(error);
-  }
-
-
+        </div>
+        </div>
+        `;
+        i++
+    }
 }
 
 load_tasks()
